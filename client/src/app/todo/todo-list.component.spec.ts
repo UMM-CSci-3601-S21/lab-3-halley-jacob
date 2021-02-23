5ec41a17ca27170f7fa355d58badc7bce232ac42
Jacob <jenne077@morris.umn.edu>
2021-02-22 19:24:41 -0600

    Create tests
    Created all of the tests for the todo service and todo list component. Also created the mock todo service.
    
    NOTE: The last commit finished the todo service. We forgot to mention this in the last commit message. Oops.
    #10
    #16
    Co-authored-by: @halleypaulson

--- client/src/app/todo/todo-list.component.spec.ts
+++ client/src/app/todo/todo-list.component.spec.ts
@@ -1,14 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockTodoService } from 'src/testing/todo.service.mock';
 import { TodoListComponent } from './todo-list.component';
+import { TodoService } from './todo.service';
+
+const COMMON_IMPORTS: any[] = [
+  FormsModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatOptionModule,
+  MatButtonModule,
+  MatInputModule,
+  MatExpansionModule,
+  MatTooltipModule,
+  MatListModule,
+  MatDividerModule,
+  MatRadioModule,
+  MatSnackBarModule,
+  BrowserAnimationsModule,
+  RouterTestingModule,
+];
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ]
+    TestBed.configureTestingModule({
+      imports: [COMMON_IMPORTS],
+      declarations: [ TodoListComponent ],
+      providers: [{ provide: TodoService, useValue: new MockTodoService() }]
     })
     .compileComponents();
   });
